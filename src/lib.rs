b949ac6e38039ba6ee9d2018604e0cb1007bd2cb
Arnold Emmanuel <arnoldemmanuel15@gmail.com>
2024-12-27 13:57:05 +0100

    Move logic to library crate

---
+++ src/lib.rs
@@ -0,0 +1,70 @@
+use flate2::read::GzDecoder;
+use flate2::write::GzEncoder;
+use flate2::Compression;
+use std::io::prelude::*;
+use std::{
+    env,
+    fs::{self, File},
+    io::Write,
+};
+pub struct Compressor {
+    pub flag: String,
+    source: String,
+    target: String,
+}
+
+impl Compressor {
+    pub fn process(mut args: env::Args) -> Result<Compressor, &'static str> {
+        let flag = match args.next() {
+            Some(s) => s,
+            None => {
+                return Err("provide a flag, `-c` or `-dc`");
+            }
+        };
+
+        let source = match args.next() {
+            Some(s) => s,
+            None => {
+                return Err("provide path to file");
+            }
+        };
+
+        let target = match args.next() {
+            Some(s) => s,
+            None => {
+                return Err("provide filename for compressed file");
+            }
+        };
+
+        Ok(Compressor {
+            flag,
+            source,
+            target,
+        })
+    }
+
+    pub fn compress_file(&self) {
+        let mut e = GzEncoder::new(File::create(&self.target).unwrap(), Compression::default());
+
+        let buf = fs::read(&self.source).unwrap();
+
+        e.write_all(&buf).unwrap();
+        e.finish().unwrap();
+
+        println!("file compressed");
+    }
+    pub fn decompress_file(&self) {
+        let buf = fs::read(&self.source).unwrap();
+
+        let mut d = GzDecoder::new(&buf[..]);
+
+        let mut df = File::create(&self.target).unwrap();
+
+        let mut s = Vec::new();
+        d.read_to_end(&mut s).unwrap();
+
+        df.write(&s).unwrap();
+
+        println!("file de-compressed");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,22 @@
 extern crate flate2;
 
-use flate2::read::GzDecoder;
-use flate2::write::GzEncoder;
-use flate2::Compression;
-use std::env;
-use std::fs;
-use std::fs::File;
-use std::io::Read;
-use std::io::Write;
+use file_compressor::Compressor;
+use std::{env, process};
 
 fn main() {
     let mut env_args = env::args();
     env_args.next();
 
-    let source = match env_args.next() {
-        Some(s) => s,
-        None => {
-            eprintln!("provide path to file");
-            return;
-        }
-    };
+    let compressor = Compressor::process(env_args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing args: {}", err);
+        process::exit(1)
+    });
 
-    let target = match env_args.next() {
-        Some(s) => s,
-        None => {
-            eprintln!("provide filename for compressed file");
-            return;
-        }
-    };
-
-    compress_file(&source, &target);
-    // decompress_file(&source, &target);
-}
-
-fn compress_file(source: &String, target: &String) {
-    let mut e = GzEncoder::new(File::create(target).unwrap(), Compression::default());
-
-    let buf = fs::read(source).unwrap();
-
-    e.write_all(&buf).unwrap();
-    e.finish().unwrap();
-
-    println!("file compressed");
-}
-
-fn decompress_file(source: &String, target: &String) {
-    let buf = fs::read(source).unwrap();
-
-    let mut d = GzDecoder::new(&buf[..]);
-
-    let mut df = File::create(target).unwrap();
-
-    let mut s = Vec::new();
-    d.read_to_end(&mut s).unwrap();
-
-    df.write(&s).unwrap();
-
-    println!("file de-compressed");
+    if compressor.flag == "-c" {
+        compressor.compress_file();
+    } else if compressor.flag == "-dc" {
+        compressor.decompress_file();
+    } else {
+        eprintln!("invalid syntax")
+    }
 }
